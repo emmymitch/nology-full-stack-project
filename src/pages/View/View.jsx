2f619eaca546e7c9acd4cc6631959f174556f287
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-09-05 17:27:33 +0100

    API get all; enhanced View page

--- src/pages/View/View.jsx
+++ src/pages/View/View.jsx
@@ -1,12 +1,27 @@
 import "./View.scss";
 import MythCardContainer from "../../containers/MythCardContainer/MythCardContainer";
+import Button from "../../components/Button/Button";
+import { useState } from "react";
 import myths from "../../data/mockMyth";
 
 const View = () => {
+    const [filteredMyths, setFilteredMyths] = useState([]);
+
+    const getMyths = async() => {
+        const response = await fetch("http://localhost:8080/myths");
+        let mythsToRender = await response.json();
+
+        setFilteredMyths(mythsToRender);
+    }
+
     return (
         <div className="view">
             <h1>View</h1>
-            <MythCardContainer mythsToShow={myths} />
+            <h3>Use this page to find the gods you want to know about.</h3>
+            <div className="view__filters">
+                <Button className={"view__button"} onClick={getMyths} text={"View All"} />
+            </div>
+            <MythCardContainer mythsToShow={filteredMyths} />
         </div>
 
     )
