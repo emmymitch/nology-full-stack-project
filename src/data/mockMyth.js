c3222a5a791037303ce053143522aa8b532d15d2
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-09-05 15:37:44 +0100

    Added mock data for styling

---
+++ src/data/mockMyth.js
@@ -0,0 +1,12 @@
+const myth = {
+    "id": 1,
+    "createdBy": "Emily",
+    "dateCreated": "05-09-2022",
+    "englishName": "Hermes",
+    "greekName": "Ἑρμης",
+    "majorDomains": ["Messengers", "Languages", "Thievery", "Trade"],
+    "identifiers": ["Caduceus", "Winged sandals"],
+    "description": "Messenger of the gods, personal herald to Zeus. Also acts as a psychopomp guiding souls to the Underworld."
+};
+
+export default myth;
