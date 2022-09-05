316040f40855df446b5dd46350e4a430a5bee54b
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-09-05 20:23:18 +0100

    Added response for data not found

--- src/pages/Delete/Delete.jsx
+++ src/pages/Delete/Delete.jsx
@@ -7,18 +7,23 @@ import { useState } from "react";
 
 const Delete = () => {
     const [chosenMyth, setChosenMyth] = useState([]);
+    const [notFound, setNotFound] = useState(false);
     const [readyToDelete, setReadyToDelete] = useState(false);
     const [deleted, setDeleted] = useState(false);
 
     const handleDeleteSearch = async(event) => {
+        setNotFound(false)
         event.preventDefault();
         const searchTerm = event.target[0].value;
         const response = await fetch(`http://localhost:8080/myth?englishName=${searchTerm}`);
-        const mythToCheck = await response.json();
 
-        if (mythToCheck){
+        if (response.ok){
+            const mythToCheck = await response.json();
             setChosenMyth(mythToCheck[0]);
             setReadyToDelete(true);
+            
+        } else {
+            setNotFound(true);
         }
     }
 
@@ -38,27 +43,37 @@ const Delete = () => {
             {!deleted && 
                 <form onSubmit={readyToDelete? deleteEntry : handleDeleteSearch}>
                     <div className="delete__check">
-                        <label htmlFor="deleteSearch" placeholder="Search by English name...">Delete...</label>
-                        <TextInput name={"deleteSearch"} />
+                        <label htmlFor="deleteSearch">Delete...</label>
+                        <TextInput name={"deleteSearch"} placeholder={"English name..."} />
                         <Button type={"submit"} text={"Submit"} disabled={readyToDelete} />
                     </div>
-                    
+
+                    {notFound && 
+                        <p className="delete__not-found">No matching entry!</p>
+                    }
+
                     {readyToDelete && 
-                        <>
-                            <p className="delete__confirm">Is this who you're trying to delete?</p>
-                            <br />
-                            <Button onClick={deleteEntry} text={"Yes"} />
-                            <Button onClick={() => {setReadyToDelete(false)}} text={"No"} />
-                            <br />
-                            <p className="delete__confirm">If not, search again</p>
-                            <MythCard myth={chosenMyth} />
-                        </>
+                        <div className="delete__changeable">
+                            <div className="delete__changeable--text">
+                                <p className="delete__confirm">Is this who you're trying to delete?</p>
+                                <br />
+                                <p className="delete__confirm">If not, click No and search again</p>
+                                <Button onClick={deleteEntry} text={"Yes"} />
+                                <Button onClick={() => {setReadyToDelete(false)}} text={"No"} />
+                                <br />
+                            </div>
+
+                            <div className="delete__changeable--myth-card">
+                               <MythCard myth={chosenMyth} /> 
+                            </div>
+                            
+                        </div>
                         }
                 </form>
             }
 
             {deleted && 
-                <p className="delete__success-message">Successfully deleted {filteredMyths.englishName}</p>
+                <p className="delete__success-message">Successfully deleted {chosenMyth.englishName}.</p>
             }
 
         </div>
